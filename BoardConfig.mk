a284a5a0ba0490cf2e0bd906c1530de62920b220
ahmedgamal <12355.ahmedgamal.com@gmail.com>
2018-06-04 14:40:33 +0200

    first commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,173 +1,71 @@
-# Copyright (C) 2014 The CyanogenMod Project
+#
+# Copyright (C) 2015 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from the proprietary version
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
 -include vendor/samsung/j1pop3g/BoardConfigVendor.mk
 
-# Platform
+# Architecture 
 TARGET_ARCH := arm
-TARGET_BOARD_PLATFORM := sc8830
-TARGET_BOARD_PLATFORM_GPU := mali-400
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_NO_BOOTLOADER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := sc7727s
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp
-BOARD_VENDOR := samsung
 
-# Config u-boot
-TARGET_NO_BOOTLOADER := true
+# define board
+TARGET_BOARD_PLATFORM := sc8830
+TARGET_BOOTLOADER_BOARD_NAME := SC7727S
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/samsung/j1pop3g/prebuilt/kernel
+# Use this if you use a prebuilt kernel
+BOARD_MKBOOTIMG_ARGS := --second device/samsung/j1pop3g/prebuilt/second.bin
+
+# lun
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun0/file
+  
+#TWRP fstab 
+TARGET_RECOVERY_FSTAB := device/samsung/j1pop3g/rootdir/twrp.fstab
+#cm specific fstab                                 -----------  use any one according to ur build flavour -----------
+#TARGET_RECOVERY_FSTAB := device/samsung/j1pop3g/rootdir/recovery.fstab
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 480x800
+
+# Partition
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2457862144
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-# Kernel
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
-BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := j1pop3g_hw02_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/j1pop3g
-
-# RIL
-BOARD_RIL_CLASS := ../../../device/samsung/j1pop3g/ril
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/j1pop3g/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/j1pop3g/bluetooth/libbt_vndcfg.txt
-
-# Wifi
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_WLAN_DEVICE_REV := bcm4343
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/p2p_supplicant_overlay.conf"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/wpa_supplicant.conf"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/wpa_supplicant_overlay.conf"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/mcs.ini"
-WIFI_BAND := 802_11_ABG
-BOARD_HAVE_SAMSUNG_WIFI := true
-
-# Hardware rendering
-BOARD_EGL_CFG := device/samsung/j1pop3g/configs/egl.cfg
-BOARD_USE_MHEAP_SCREENSHOT := true
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-HWUI_COMPILE_FOR_PERF := true
-USE_SPRD_HWCOMPOSER := true
-USE_OPENGL_RENDERER := true
-USE_OVERLAY_COMPOSER_GPU := true
-DEVICE_FORCE_VIDEO_GO_OVERLAYCOMPOSER := true
-COMMON_GLOBAL_CFLAGS += -DSC8830_HWC
-
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Resolution
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
-
-# Audio
-BOARD_USES_TINYALSA_AUDIO := true
-BOARD_USES_SS_VOIP := true
-BOARD_USE_LIBATCHANNEL_WRAPPER := true
-
-# Media
-COMMON_GLOBAL_CFLAGS += -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
-COMMON_GLOBAL_CFLAGS += -DBOARD_CANT_REALLOCATE_OMX_BUFFERS
-
-# Board specific features
-BOARD_USE_SAMSUNG_COLORFORMAT := true
-BOARD_NEEDS_MEMORYHEAPION_SPRD := true
-COMMON_GLOBAL_CFLAGS += -DSPRD_HARDWARE
-
-# healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd-j1pop3g.sc8830
-
-# Use the CM PowerHAL
-TARGET_USES_CM_POWERHAL := true
-CM_POWERHAL_EXTENSION := sc8830
-TARGET_POWERHAL_VARIANT := cm
-
-# Init
-TARGET_NR_SVC_SUPP_GIDS := 24
-TARGET_PROVIDES_INIT_RC := true
-
-# Recovery
-BOARD_HAS_NO_REAL_SDCARD := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := SM-J110H,J1POP3G,J1POP3GJV
-
-# SELinux
-#BOARD_SEPOLICY_DIRS += device/samsung/j1pop3g/sepolicy
-#BOARD_SEPOLICY_UNION :=	\
-#	file.te	\
-#	file_contexts \
-#	seapp_contexts \
-#	theme.te \
-#	healthd.te \
-#	init.te \
-#	init_shell.te \
-#	installd.te \
-#	netd.te \
-#	shell.te \
-#	system.te \
-#	untrusted_app.te \
-#	vold.te	\
-#	zygote.te
-
-# Use dmalloc() for such low memory devices like us
-MALLOC_IMPL := dlmalloc
-
-# Use prebuilt webviewchromium to cut down build time
-PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
-
-# Enable dex-preoptimization to speed up the first boot sequence
-WITH_DEXPREOPT := true
-#WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-WITH_DEXPREOPT_PIC := true
-WITH_DEXPREOPT_COMP := false
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
-BACKLIGHT_PATH := /sys/class/backlight/panel/brightness
 
 
-# CMHW
-BOARD_HARDWARE_CLASS := device/samsung/j1pop3g/cmhw/
-
-# TWRP
-##RECOVERY_VARIANT := twrp
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
+# Twrp configs
+TW_THEME := portrait_mdpi
 SP1_NAME := "internal_sd"
 SP1_BACKUP_METHOD := files
 SP1_MOUNTABLE := 1
@@ -178,15 +76,7 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_FLASH_FROM_STORAGE := true
 TW_NO_REBOOT_BOOTLOADER := true
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/platform/sec-thermistor/temperature"
-TWHAVE_SELINUX := true
-TARGET_RECOVERY_INITRC := device/samsung/j1pop3g/etc/init.rc
-TARGET_RECOVERY_FSTAB := device/samsung/j1pop3g/rootdir/recovery.fstab
-TW_HAS_DOWNLOAD_MODE := true
-DEVICE_RESOLUTION := 480x800
-TW_THEME := portrait_mdpi
+TW_MTP_DEVICE := /dev/usb_mtp_gadget
+BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
-
-# UMS
-BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
--- cm.mk
+++ cm.mk
@@ -1,19 +1,33 @@
-## Specify phone tech before including full_phone	
-$(call inherit-product, vendor/cm/config/telephony.mk)
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Release name
-PRODUCT_RELEASE_NAME := j1pop3g
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+#$(call inherit-product, device/samsung/j1pop3g/device.mk)
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_j1pop3g.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/samsung/j1pop3g/device.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := j1pop3g
 PRODUCT_NAME := cm_j1pop3g
+PRODUCT_DEVICE := j1pop3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J110H
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := phone
